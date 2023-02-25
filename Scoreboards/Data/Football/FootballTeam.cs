aa438389797d00bec65e77408361fb7f1a0fa5cb
Miloš Jovanović <milosh-96@outlook.com>
2023-02-25 01:39:32 +0100

    Add project files.

---
+++ Scoreboards/Data/Football/FootballTeam.cs
@@ -0,0 +1,8 @@
+﻿namespace Scoreboards.Data.Football
+{
+    public class FootballTeam
+    {
+        public string Name { get; set; } = "Undefined";
+        public int Score { get; set; } = 0;
+    }
+}
---
+++ Scoreboards/Models/Football/FootballScoreboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Scoreboards.Data.Football;
+
+namespace Scoreboards.Models.Football
+{
+    public class FootballScoreboardViewModel
+    {
+        public FootballTeam HomeTeam { get; set; } = new FootballTeam();
+        public FootballTeam AwayTeam { get; set; } = new FootballTeam();
+
+    
+
+        public int Clock { get; set; } = 89*60;
+    }
+}
