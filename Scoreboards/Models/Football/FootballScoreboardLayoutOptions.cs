73ac74528c6e8ddd245c1efa16967ab79b73ebae
Miloš Jovanović <milosh-96@outlook.com>
2023-03-05 03:57:02 +0100

    landing page

--- Scoreboards/Models/Football/FootballScoreboardLayoutOptions.cs
+++ Scoreboards/Models/Football/FootballScoreboardLayoutOptions.cs
@@ -4,7 +4,7 @@
     {
         public bool ShowScore { get; set; } = true;
         public bool ShowClock { get; set; } = true;
-        public bool ShowTeamNames { get; set; } = true;
+        public bool ShowTeamNames { get; set; } = false;
         public bool ShowTeamLogos { get; set; } = true;
         public bool ShowLineups { get; set; } = true;
         public bool ShowStats { get; set; } = false;
--- Scoreboards/SiteInfo.cs
+++ Scoreboards/SiteInfo.cs
@@ -3,5 +3,7 @@
     public static class SiteInfo
     {
         public  static string Name { get; set; } = "R8 Match Scoreboards";
+        public static string Description { get; set; } = "Free Live scoreboards for your tournaments.  Real time updates, stats, lineups and various styling options. Or just use it for fun, and be a scoreboard operator with presets that are inspired by" +
+            " stadiums such as Anfield, Santiago Bernabeu and others...";
     }
 }
