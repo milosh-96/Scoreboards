d880d87ae81451603721cf457283a53d2472eac1
Miloš Jovanović <milosh-96@outlook.com>
2023-03-30 05:23:35 +0200

    add animate.css and some options regarding events

---
+++ Scoreboards/Areas/App/Pages/Output/Football/Components/EventOutputOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace Scoreboards.Areas.App.Pages.Output.Football.Components
+{
+    public class EventOutputOptions
+    {
+        public bool FullScreenMode { get; set; } = false;
+
+        /// <summary>
+        /// this should be class that will change animation, for example - from bounce to heartbeat.
+        /// </summary>
+        /// <remarks>
+        /// Multiple CSS classes (separated by a space) are allowed, but use them only
+        /// if combined classes will result in different behaviour of the animation, not 
+        /// for styling purposes.
+        /// </remarks>
+        public string AnimationCssClass { get; set; } = "animate__heartBeat";
+    }
+}
