fb7ca586deea7403e0f5baa0f95a963940057dd4
Miloš Jovanović <milosh-96@outlook.com>
2023-05-19 04:27:17 +0200

    title

--- Scoreboards/SiteInfo.cs
+++ Scoreboards/SiteInfo.cs
@@ -3,7 +3,8 @@
     public static class SiteInfo
     {
         public  static string Name { get; set; } = "R8 Match Scoreboards";
-        public static string Description { get; set; } = "Free Live scoreboards for your tournaments.  Real time updates, stats, lineups and various styling options. Or just use it for fun, and be a scoreboard operator with presets that are inspired by" +
+        public  static string Title { get; set; } = "R8 Match Scoreboards - Free Online Scoreboard Software";
+        public static string Description { get; set; } = "Free Online Scoreboard Software (app) for your tournaments.  Real time updates, stats, lineups and various styling options. Or just use it for fun, and be a scoreboard operator with presets that are inspired by" +
             " stadiums such as Anfield, Santiago Bernabeu and others...";
     }
 }
